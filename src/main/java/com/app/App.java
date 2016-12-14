410d49b80bb1d4b62d9935207790332cffb8d010
makothari <kotharimayur99@gmail.com>
2016-12-14 12:45:32 +0530

    Update App.java

--- src/main/java/com/app/App.java
+++ src/main/java/com/app/App.java
@@ -3,11 +3,10 @@ package com.app;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@Configuration
-@EnableAutoConfiguration
+
+@SpringBootApplication
 public class App {
 	private static Logger logger = LoggerFactory.getLogger(App.class);
 
